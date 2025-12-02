4c3828cf1cdbb9bdf7f491a0a47ce432450f206a
chami-gunawardane <chamigunawardane@gmail.com>
2025-12-02 09:46:57 +0530

    Images added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Search from "./components/Search";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
-    </>
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper" >
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1> Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+
+        < Search  />
+      </div>
+    </main>
   );
 }
 
