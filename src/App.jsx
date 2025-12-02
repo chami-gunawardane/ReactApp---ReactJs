4a8b807f8e4746fa91bdcc895d4b556c919ff436
chami-gunawardane <chamigunawardane@gmail.com>
2025-12-02 10:07:33 +0530

    Header Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Search from "./components/Search";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <main>
       <div className="pattern" />
@@ -13,7 +14,8 @@ function App() {
           <h1> Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
         </header>
 
-        < Search  />
+        < Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1 className="text-white">{searchTerm}</h1>
       </div>
     </main>
   );
